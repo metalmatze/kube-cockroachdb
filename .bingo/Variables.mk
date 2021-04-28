db60a40236b1db4a122088c69166bcaff6db3ead
Thomas Boerger <thomas@webhippie.de>
2021-04-28 23:01:33 +0200

    Update all bingo dependencies and use .bingo/Variables.mk
    Before this PR there had been custom tasks defined within the Makefile,
    I have replaced them by the variables defined within bingo.

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -1,10 +1,11 @@
-# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.2.2. DO NOT EDIT.
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.4.0. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
+BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 GOPATH ?= $(shell go env GOPATH)
 GOBIN  ?= $(firstword $(subst :, ,${GOPATH}))/bin
 GO     ?= $(shell which go)
 
-# Bellow generated variables ensure that every time a tool under each variable is invoked, the correct version
+# Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
 # For example for controller-gen variable:
 #
@@ -16,33 +17,33 @@ GO     ?= $(shell which go)
 #	@echo "Running controller-gen"
 #	@$(CONTROLLER_GEN) <flags/args..>
 #
-CONTROLLER_GEN := $(GOBIN)/controller-gen-v0.3.0
-$(CONTROLLER_GEN): .bingo/controller-gen.mod
+CONTROLLER_GEN := $(GOBIN)/controller-gen-v0.5.0
+$(CONTROLLER_GEN): $(BINGO_DIR)/controller-gen.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/controller-gen-v0.3.0"
-	@cd .bingo && $(GO) build -modfile=controller-gen.mod -o=$(GOBIN)/controller-gen-v0.3.0 "sigs.k8s.io/controller-tools/cmd/controller-gen"
+	@echo "(re)installing $(GOBIN)/controller-gen-v0.5.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=controller-gen.mod -o=$(GOBIN)/controller-gen-v0.5.0 "sigs.k8s.io/controller-tools/cmd/controller-gen"
 
 EMBEDMD := $(GOBIN)/embedmd-v1.0.0
-$(EMBEDMD): .bingo/embedmd.mod
+$(EMBEDMD): $(BINGO_DIR)/embedmd.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/embedmd-v1.0.0"
-	@cd .bingo && $(GO) build -modfile=embedmd.mod -o=$(GOBIN)/embedmd-v1.0.0 "github.com/campoy/embedmd"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=embedmd.mod -o=$(GOBIN)/embedmd-v1.0.0 "github.com/campoy/embedmd"
 
-GOJSONTOYAML := $(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c
-$(GOJSONTOYAML): .bingo/gojsontoyaml.mod
+GOJSONTOYAML := $(GOBIN)/gojsontoyaml-v0.0.0-20201216083616-202f76bf8c1f
+$(GOJSONTOYAML): $(BINGO_DIR)/gojsontoyaml.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c"
-	@cd .bingo && $(GO) build -modfile=gojsontoyaml.mod -o=$(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c "github.com/brancz/gojsontoyaml"
+	@echo "(re)installing $(GOBIN)/gojsontoyaml-v0.0.0-20201216083616-202f76bf8c1f"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=gojsontoyaml.mod -o=$(GOBIN)/gojsontoyaml-v0.0.0-20201216083616-202f76bf8c1f "github.com/brancz/gojsontoyaml"
 
-JSONNET := $(GOBIN)/jsonnet-v0.16.0
-$(JSONNET): .bingo/jsonnet.mod
+JSONNET := $(GOBIN)/jsonnet-v0.17.0
+$(JSONNET): $(BINGO_DIR)/jsonnet.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/jsonnet-v0.16.0"
-	@cd .bingo && $(GO) build -modfile=jsonnet.mod -o=$(GOBIN)/jsonnet-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnet"
+	@echo "(re)installing $(GOBIN)/jsonnet-v0.17.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=jsonnet.mod -o=$(GOBIN)/jsonnet-v0.17.0 "github.com/google/go-jsonnet/cmd/jsonnet"
 
-JSONNETFMT := $(GOBIN)/jsonnetfmt-v0.16.0
-$(JSONNETFMT): .bingo/jsonnetfmt.mod
+JSONNETFMT := $(GOBIN)/jsonnetfmt-v0.17.0
+$(JSONNETFMT): $(BINGO_DIR)/jsonnetfmt.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/jsonnetfmt-v0.16.0"
-	@cd .bingo && $(GO) build -modfile=jsonnetfmt.mod -o=$(GOBIN)/jsonnetfmt-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnetfmt"
+	@echo "(re)installing $(GOBIN)/jsonnetfmt-v0.17.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=jsonnetfmt.mod -o=$(GOBIN)/jsonnetfmt-v0.17.0 "github.com/google/go-jsonnet/cmd/jsonnetfmt"
 
